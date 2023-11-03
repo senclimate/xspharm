800ded719ea122af45f9dfa52752802b772ec23e
Sen Zhao <zhaos2016@gmail.com>
2023-11-03 10:16:08 -1000

    doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -19,10 +19,6 @@
 #
 import os
 import sys
-import subprocess
-
-# Ensure numpy is installed before proceeding
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
 
 sys.path.insert(0, os.path.abspath('..'))
 
@@ -94,7 +90,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
